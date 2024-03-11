2a1a6998255d4ff98dedf4d03f1edde5d789fca8
NigamAnanya <as1450@srmist.edu.in>
2024-03-12 04:21:40 +0530

    updated

---
+++ frontend/pages/landing.tsx
@@ -0,0 +1,35 @@
+import { GetServerSideProps } from 'next';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const landing = () => {
+  //state variables
+  const[songs, setsongs] = useState([]);
+
+  const getSongs = async()=>{
+    const response = await axios
+      .get(
+      `http://localhost:3000/getSongs`
+    )
+    console.log(response)
+
+      }
+
+  useEffect(()=>{
+     
+
+  },[])
+
+
+  return (
+    <div>
+      <input className = "text-black" type="text" name="Search Songs" id="text" />
+      <button>Search</button>
+    </div>
+  );
+}
+
+
+
+export default landing
